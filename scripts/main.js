902b5c0b818cb0c3cf0b001d83ba2e8a591e338e
Tobias Rauer <prototowb@gmail.com>
2020-07-11 00:29:47 +0200

    nav hover effects adjustment

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 
 
 /* DROPDOWN */
-// Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict
+// Loop through all dropdown buttons to toggle between hiding and showing its dropdown content
 var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
